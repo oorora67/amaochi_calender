05cbca5801057d470b58629560961a194db195b9
oorora <oorora0067@gmail.com>
2022-01-23 17:06:54 +0900

    commit

--- data/js/clock.js
+++ data/js/clock.js
@@ -1,81 +1,52 @@
 const clock = () => {
-    // 現在の日時・時刻の情報を取得
     const d = new Date();
-  
-    // 年を取得
     let year = d.getFullYear();
-    // 月を取得
     let month = d.getMonth() + 1;
-    // 日を取得
     let date = d.getDate();
-    // 曜日を取得
     let dayNum = d.getDay();
     const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     let day = weekday[dayNum];
-    // 時を取得
     let hour = d.getHours();
-    // 分を取得
     let min = d.getMinutes();
-    // 秒を取得
     let sec = d.getSeconds();
-  
-    // 1桁の場合は0を足して2桁に
     month = month < 10 ? "0" + month : month;
     date = date < 10 ? "0" + date : date;
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
-  
-    // 日付・時刻の文字列を作成
     let today = `${year}.${month}.${date} ${day}`;
     let time = `${hour}:${min}:${sec}`;
-  
-    // 文字列を出力
     document.querySelector(".clock-date").innerText = today;
     document.querySelector(".clock-time").innerText = time;
-  };
-  
-  // 1秒ごとにclock関数を呼び出す
-  setInterval(clock, 1000);
+};
+setInterval(clock, 1000);
 document.getElementById("button").onclick = function(e) {
     if(document.querySelector(".button-text").innerText == "PlayNow."){
         return;
     };
     document.querySelector(".button-text").innerText = "PlayNow.";
     let playlist = new Array();
-    // 現在の日時・時刻の情報を取得
     const d = new Date();
-    // 年を取得
     let year = d.getFullYear();
-    // 月を取得
     let month = d.getMonth() + 1;
-    // 日を取得
     let date = d.getDate();
-    // 曜日を取得
     let dayNum = d.getDay();
     const weekday = ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"];
     let day = weekday[dayNum];
-    // 時を取得
     let hour = d.getHours();
-    // 分を取得
     let min = d.getMinutes();
-    // 秒を取得
     let sec = d.getSeconds();
-    // 1桁の場合は0を足して2桁に
     month = month < 10 ? "0" + month : month;
     date = date < 10 ? "0" + date : date;
     hour = hour < 10 ? "0" + hour : hour;
     min = min < 10 ? "0" + min : min;
     sec = sec < 10 ? "0" + sec : sec;
-    //日付音声
     let month_s = String(month).split('');
     let date_s = String(date).split('');
     switch(month_s[0]){
         case '0':
-            
             break;
         case '1':
-            
             playlist.push('data/sound/10.mp3');
             break;
         default:
@@ -83,42 +54,32 @@ document.getElementById("button").onclick = function(e) {
     }
     switch(month_s[1]){
         case '0':
-            
             break;
         case '1':
-            
             playlist.push('data/sound/1.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/2.mp3');
             break;
         case '3':
-            
             playlist.push('data/sound/3.mp3');
             break;
         case '4':
-            
             playlist.push('data/sound/4.mp3');
             break;
         case '5':
-            
             playlist.push('data/sound/5.mp3');
             break;
         case '6':
-            
             playlist.push('data/sound/6.mp3');
             break;
         case '7':
-            
             playlist.push('data/sound/7.mp3');
             break;
         case '8':
-            
             playlist.push('data/sound/8.mp3');
             break;
         case '9':
-            
             playlist.push('data/sound/9.mp3');
             break;
         default:
@@ -127,18 +88,14 @@ document.getElementById("button").onclick = function(e) {
     playlist.push('data/sound/月.mp3');
     switch(date_s[0]){
         case '0':
-            
             break;
         case '1':
-            
             playlist.push('data/sound/10.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/20.mp3');
             break;
         case '3':
-            
             playlist.push('data/sound/30.mp3');
             break;
         default:
@@ -146,42 +103,32 @@ document.getElementById("button").onclick = function(e) {
     }
     switch(date_s[1]){
         case '0':
-            
             break;
         case '1':
-            
             playlist.push('data/sound/1.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/2.mp3');
             break;
         case '3':
-            
             playlist.push('data/sound/3.mp3');
             break;
         case '4':
-            
             playlist.push('data/sound/4.mp3');
             break;
         case '5':
-            
             playlist.push('data/sound/5.mp3');
             break;
         case '6':
-            
             playlist.push('data/sound/6.mp3');
             break;
         case '7':
-            
             playlist.push('data/sound/7.mp3');
             break;
         case '8':
-            
             playlist.push('data/sound/8.mp3');
             break;
         case '9':
-            
             playlist.push('data/sound/9.mp3');
             break;
         default:
@@ -190,51 +137,39 @@ document.getElementById("button").onclick = function(e) {
     playlist.push('data/sound/日.mp3');
     switch(day){
         case 'SUN':
-            
             playlist.push('data/sound/日曜日.mp3');
             break;
         case 'MON':
-            
             playlist.push('data/sound/月曜日.mp3');
             break;
         case 'TUE':
-            
             playlist.push('data/sound/火曜日.mp3');
             break;
         case 'WED':
-            
             playlist.push('data/sound/水曜日.mp3');
             break;
         case 'THU':
-            
             playlist.push('data/sound/木曜日.mp3');
             break;
         case 'FRI':
-            
             playlist.push('data/sound/金曜日.mp3');
             break;
         case 'SAT':
-            
             playlist.push('data/sound/土曜日.mp3');
             break;
         default:
             
     }
-    //時間音声
     let hour_s = String(hour).split('');
     let min_s = String(min).split('');
     let sec_s = String(sec).split('');
-    
     switch(hour_s[0]){
         case '0':
-            
             break;
         case '1':
-            
             playlist.push('data/sound/10.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/20.mp3');
             break;
         default:
@@ -242,73 +177,56 @@ document.getElementById("button").onclick = function(e) {
     }
     switch(hour_s[1]){
         case '0':
-            
             if(hour_s[0]==0){
                 playlist.push('data/sound/0.mp3');
             }
             break;
         case '1':
-            
             playlist.push('data/sound/1.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/2.mp3');
             break;
         case '3':
-            
             playlist.push('data/sound/3.mp3');
             break;
         case '4':
-            
             playlist.push('data/sound/4.mp3');
             break;
         case '5':
-            
             playlist.push('data/sound/5.mp3');
             break;
         case '6':
-            
             playlist.push('data/sound/6.mp3');
             break;
         case '7':
-            
             playlist.push('data/sound/7.mp3');
             break;
         case '8':
-            
             playlist.push('data/sound/8.mp3');
             break;
         case '9':
-            
             playlist.push('data/sound/9.mp3');
             break;
         default:
-            
     }
     playlist.push('data/sound/時.mp3');
     switch(min_s[0]){
         case '0':
-            
             break;
         case '1':
-            
             playlist.push('data/sound/10.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/20.mp3');
             break;
         case '3':
-            
             playlist.push('data/sound/30.mp3');
             break;
         case '4':
-            
             playlist.push('data/sound/40.mp3');
             break;
         case '5':
-            
             playlist.push('data/sound/50.mp3');
             break;
         default:
@@ -316,59 +234,46 @@ document.getElementById("button").onclick = function(e) {
     }
     switch(min_s[1]){
         case '0':
-            
             if(min_s[0]==0){
                 playlist.push('data/sound/0.mp3');
             }
             break;
         case '1':
-            
             playlist.push('data/sound/1.mp3');
             break;
         case '2':
-            
             playlist.push('data/sound/2.mp3');
             break;
         case '3':
-            
             playlist.push('data/sound/3.mp3');
             break;
         case '4':
-            
             playlist.push('data/sound/4.mp3');
             break;
         case '5':
-            
             playlist.push('data/sound/5.mp3');
             break;
         case '6':
-            
             playlist.push('data/sound/6.mp3');
             break;
         case '7':
-            
             playlist.push('data/sound/7.mp3');
             break;
         case '8':
-            
             playlist.push('data/sound/8.mp3');
             break;
         case '9':
-            
             playlist.push('data/sound/9.mp3');
             break;
         default:
-            
     }
     playlist.push('data/sound/分.mp3');
-    
     var audio = document.createElement('audio');
     document.body.appendChild(audio);
     audio.style.width = '100%';
     audio.style.height = 'auto';
     audio.controls = true;
     audio.volume = 1.0;
-
     audio.src = playlist[0];
     audio.play();
     let index = 0;
@@ -383,7 +288,6 @@ document.getElementById("button").onclick = function(e) {
         document.querySelector(".button-text").innerText = "SOUND";
     }
     });
-
 };
 
 document.getElementById("url-button").onclick = async function() {
