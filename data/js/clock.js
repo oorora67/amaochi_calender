a8d5ee2e5b55c4672628f171b6a6c0992cc12f81
oorora <oorora0067@gmail.com>
2022-11-14 18:32:22 +0900

    Update clock.js

--- data/js/clock.js
+++ data/js/clock.js
@@ -325,7 +325,6 @@ function getEvent_voice(date) {
 function convertCSVtoArray(str, date) { // 読み込んだCSVデータが文字列として渡される
     var result = []; // 最終的な二次元配列を入れるための配列
     var tmp = str.toString().split("\r\n"); // 改行を区切り文字として行を要素とした配列を生成
-    console.log(tmp);
     let ret = 'data/sound/blank.mp3';
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for (var i = 0; i < tmp.length; ++i) {
