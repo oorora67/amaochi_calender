ef6cc7da651685d06d61e24e6a3005f3aadb24fb
oorora <oorora0067@gmail.com>
2022-11-14 18:31:35 +0900

    Update clock.js

--- data/js/clock.js
+++ data/js/clock.js
@@ -276,7 +276,7 @@ document.getElementById("button").onclick = function (e) {
         playlist.push(random[Math.floor(Math.random() * random.length)]);
     };
     if (cb4.checked) {
-        playlist.push(getEvent_voice(month + date));
+        playlist.push(getEvent_voice(("" + month) + date));
     };
     var audio = document.createElement('audio');
     playlist.push('data/sound/blank.mp3');
@@ -324,7 +324,8 @@ function getEvent_voice(date) {
 // 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
 function convertCSVtoArray(str, date) { // 読み込んだCSVデータが文字列として渡される
     var result = []; // 最終的な二次元配列を入れるための配列
-    var tmp = str.toString().split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+    var tmp = str.toString().split("\r\n"); // 改行を区切り文字として行を要素とした配列を生成
+    console.log(tmp);
     let ret = 'data/sound/blank.mp3';
     // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
     for (var i = 0; i < tmp.length; ++i) {
